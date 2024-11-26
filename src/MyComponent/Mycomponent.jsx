d736ad007a719a2f723aa7af8e36831d7c37a0c1
duyzo <140488871+duyzo@users.noreply.github.com>
2024-11-26 15:36:19 +0700

    video 31

--- src/MyComponent/Mycomponent.jsx
+++ src/MyComponent/Mycomponent.jsx
@@ -1,11 +1,13 @@
 // React.Fragment
 import './style.css'
-
+let sayHi = "Hello Duy Dz"
+let age = 19
 let MyComponent = () => {
     return (
         <> 
         <div>MyComponent Import</div>
-        <div className="JSX">Test</div>
+        <div className="JSX">Test {sayHi} {age}</div>
+        <div>{console.log("Hello Duy Dz")}</div>
         </>
     )
   }
